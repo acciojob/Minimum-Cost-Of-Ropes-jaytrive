0b5373ee849252df422101b10e5a44cf76089872
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-02-19 14:42:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function CostRope(event) {
 	var cost=0
 	while (arr.length>1) {
 		var res=Number(arr[0])+Number(arr[1])
+		arr.shift()
+		arr.shift()
 		arr.push(res)
 		cost+=res
-		arr.shift()
-		arr.shift()
 		arr.sort(function(a,b){
 			return a-b
 		})
