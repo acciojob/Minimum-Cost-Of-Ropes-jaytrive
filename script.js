5966f558a32fb80eea2f1a14e285e7e2cd6cb192
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-02-19 13:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function CostRope(event) {
+	event.preventDefault()
+	var input=document.getElementsByTagName("input")[0].value
+	var arr=input.split(',')
+	arr.sort(function(a,b){
+		return a-b
+	})
+	var cost=0
+	while (arr.length>1) {
+		var res=Number(arr[0])+Number(arr[1])
+		arr.push(res)
+		cost+=res
+		arr.shift()
+		arr.shift()
+		arr.sort(function(a,b){
+			return a-b
+		})
+	}
+	document.getElementById('result').innerText=cost
+}
