9624cd05747e517393bbb2f92f4457409a87cb81
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-02-19 14:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //your code here
 function CostRope(event) {
-	event.preventDefault()
-	var input=document.getElementsByTagName("input")[0].value
-	var arr=input.split(',')
+	event.preventDefault();
+	var firstinput=document.querySelector("input").value;
+    var arr=firstinput.split(",");
+    arr.sort(function (a, b) {
+    return a - b;
+  });
+
+var cost=0;
+while(arr.length>1){
+	var result=Number(arr[0])+Number(arr[1]);
+	arr.splice(0,2);
+	arr.push(result);
+	cost+=result;
 	arr.sort(function(a,b){
-		return a-b
-	})
-	var cost=0
-	while (arr.length>1) {
-		var res=Number(arr[0])+Number(arr[1])
-		arr.shift()
-		arr.shift()
-		arr.push(res)
-		cost+=res
-		arr.sort(function(a,b){
-			return a-b
-		})
-	}
-	document.getElementById('result').textContent=cost
+		return a-b;
+	});
+}
+document.getElementById("result").textContent=cost;	
 }
